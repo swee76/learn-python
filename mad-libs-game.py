11a0842cdc379a141de15479409064dd24d9102b
swee76 <oshinikithma1213@gmail.com>
2022-12-01 15:53:30 +0530

    mad lib game

---
+++ mad-libs-game.py
@@ -0,0 +1,9 @@
+# Mad libs game - Random words create a story
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
