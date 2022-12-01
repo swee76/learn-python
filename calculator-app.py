f5df687a5e79cc8c694cac85b762d0afab234a5e
swee76 <oshinikithma1213@gmail.com>
2022-12-01 15:41:04 +0530

    Calculator app

---
+++ calculator-app.py
@@ -0,0 +1,12 @@
+# Basic calculator app
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+result = float(num1) + float(num2)
+
+print(result)
+
+# By default, python all inputs as string.
+# So to do apply any arithmatic formula, we have to convert that input into int or float
+
