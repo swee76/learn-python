67c036da7bc3a3b10268285bda348cbb8a86145a
swee76 <oshinikithma1213@gmail.com>
2022-12-01 15:29:37 +0530

    get input from user

--- getting-started.py
+++ getting-started.py
@@ -13,13 +13,14 @@ character_name = "John"
 character_age = 35
 is_male = True
 
-# print("There once was a man named " + character_name + ", ")
-# print("He was " + {character_age} + " years old. ")
-#
-# character_name = "Mike"
-#
-# print("He really liked the name " + character_name + ", ")
-# print("But didn't like being " + {character_age} + ".")
+# To print Number next to the string use str() function
+print("There once was a man named " + character_name + ", ")
+print("He was " + str(character_age) + " years old. ")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print("But didn't like being " + str(character_age) + ".")
 
 phrase = "Code Academy"
 print(phrase.upper()) # in javascript this will be phrase.toUppsercase()
--- working-with-numbers.py
+++ working-with-numbers.py
@@ -1,3 +1,5 @@
+from math import *
+
 # working with numbers
 print(2 * 4 + 5)
 
@@ -5,3 +7,28 @@ print(2 * 4 + 5)
 my_num = 5
 print(str(my_num))
 # in Javascript this will be my_num.toString() or String(my_num)
+
+negative_num = -3
+print(abs(negative_num))
+# gives absolute value of negative number
+
+print(pow(3, 2))
+# in Javascript this will be Math.pow(3, 2)
+
+print(max(5, 6))
+# in Javascript this will be Math.max(5, 6)
+
+print(min(5, 6))
+# in Javascript this will be Math.min(5, 6)
+
+print(round(2.53))
+# in Javascript this will be Math.round(2.53)
+
+print(floor(3.26))
+# in Javascript this will be Math.floor(3.26)
+
+print(ceil(4.65))
+# in Javascript this will be Math.ceil(4.65)
+
+print(sqrt(256))
+# in Javascript this will be Math.sqrt(256)
