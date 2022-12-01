131f5e801c6b8600514a0545794ddb406989b8fc
Osini <osini.kithma@atlaslabs.com.au>
2022-12-01 14:35:30 +0530

    Initial commit

---
+++ getting-started.py
@@ -0,0 +1,45 @@
+print("Hello World!")
+
+print("Print a shape")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+print("Learn Data & Variables")
+
+character_name = "John"
+character_age = 35
+is_male = True
+
+# print("There once was a man named " + character_name + ", ")
+# print("He was " + {character_age} + " years old. ")
+#
+# character_name = "Mike"
+#
+# print("He really liked the name " + character_name + ", ")
+# print("But didn't like being " + {character_age} + ".")
+
+phrase = "Code Academy"
+print(phrase.upper()) # in javascript this will be phrase.toUppsercase()
+
+print(phrase.lower().islower())
+# in javascript there is no islower() or isupper() checker
+
+print(len(phrase))
+# in javascript this will be phrase.length
+
+print(phrase[5])
+# in javascript this is same, any index char value can be obtained
+
+print(phrase[len(phrase) - 1])
+# in javascript this will be phrase[phrase.length - 1]
+
+print(phrase.index("d"))
+# in javascript this will be phrase.indexOf("d")
+
+print(phrase.replace("Code","British"))
+# in javascript this is same
+
+
---
+++ working-with-numbers.py
@@ -0,0 +1,7 @@
+# working with numbers
+print(2 * 4 + 5)
+
+# convert to a string
+my_num = 5
+print(str(my_num))
+# in Javascript this will be my_num.toString() or String(my_num)
