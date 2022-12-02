ce9ec6a3c4f1baedc7d64e84743ba30c05d351c8
swee76 <oshinikithma1213@gmail.com>
2022-12-02 14:24:21 +0530

    tuples

---
+++ learn-tuples.py
@@ -0,0 +1,14 @@
+# tuples
+# Immutable list
+# cannot change once initialized
+
+coordinates = (4, 5)
+print(coordinates[0])
+
+# If you try to assign a value to this tuple, it prompts an error
+
+tupleArray = [(3, 5), (67, 89), (2, 5)]
+tupleArray[2] = (34, 5)
+print(tupleArray)
+
+# list can modify, but tuples cannot modify
