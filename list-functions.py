610cd36782f524ba88abbdd67466507334e16d9b
swee76 <oshinikithma1213@gmail.com>
2022-12-02 12:28:15 +0530

    list functions 2

--- list-functions.py
+++ list-functions.py
@@ -1,6 +1,6 @@
 # Array Functions
 
-lucky_numbers = [4, 8, 15, 16, 23, 42]
+lucky_numbers = [14, 8, 15, 16, 23, 42]
 friends = ["Kevin", "Keren", "Jim", "Oscar", "Toby"]
 
 friends.extend(lucky_numbers)
@@ -11,6 +11,7 @@ friends.append("Creed")
 print(friends)
 # in javascript this will be friends.pussh("Creed")
 
+friends = ["Kevin", "Keren", "Jim", "Oscar", "Toby"]
 friends.insert(1, "Kelly")
 print(friends)
 # in javascript this will be friends.splice(1,0,"Kelly)
@@ -20,3 +21,48 @@ print(friends)
 
 # if we put friends.splice(1,1,"Kelly")
 # This replaces the value with given position value
+
+friends.remove("Jim")
+print(friends)
+# in Javascript this will be friends.splice(2,1,"Jim") - this remove the element but have to give the index
+# To remove element by only giving the value of the element use filter()
+# friends.filter((person)=> person != "Jim")
+
+friends.pop()
+print(friends)
+# This removes the last element of the array same as the Javascript
+
+# friends.clear()
+# print(friends)
+# remove every single item from the array (whole)
+
+friends = ["Kevin", "Keren", "Jim", "Oscar", "Toby"]
+print(friends.index("Toby"))
+# in javascript this will be friends.indexOf("Toby")
+
+# Add a same named element to the list
+friends.append("Oscar")
+print(friends)
+
+print(friends.count("Oscar"))
+# This returns the no of items that have the given same value
+
+fruits = ["Banana", "Mango", "Apple", "Pineapple", "Papaya"]
+fruits.sort()
+print(sorted(fruits))
+# This cannot be typed in same line as print(sorted(fruits.sort()))
+# we have to sort the list in a new line and print the sorted list in another line by using sorted(array_name)
+# Otherwise if we try to print just after sort() the list it prints None
+
+lucky_numbers.sort()
+print(sorted(lucky_numbers))
+
+lucky_numbers = [14, 8, 315, 16, 23, 42]
+lucky_numbers.reverse()
+print(lucky_numbers)
+# This reverse() the array from Last to first element without sorting
+# Javascript also have reverse()
+
+friend_list = friends.copy()
+print("Copied Array: " + str(friend_list))
+# in javascript this will be friendList = friends.copyWithin()
