3e994eb3c5edd377c352b069c8394ef01012a676
Osini Kithma <66475844+swee76@users.noreply.github.com>
2022-12-03 19:03:08 +0530

    for loop

---
+++ for-loop.py
@@ -0,0 +1,15 @@
+# for-loop
+
+friends = ["Jim", "Kevin", "Karen"]
+
+for friend in friends:
+    print(friend)
+
+for index in range(10):
+    print(index)
+
+for index in range(3, 10):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
