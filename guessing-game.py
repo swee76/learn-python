eb762cff17f7400c09bfc82880436d849ffec52c
Osini Kithma <66475844+swee76@users.noreply.github.com>
2022-12-03 18:16:07 +0530

    guessing game

---
+++ guessing-game.py
@@ -0,0 +1,20 @@
+# guessing game
+
+secret_word = "giraffe"
+
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not out_of_guesses:
+    if guess_count < guess_limit:
+        guess = input("Input your guess word: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Out of guesses, You lose!")
+else:
+    print("You win!")
