61d476f790d77e0df14d326abeefe672ca41952a
swee76 <oshinikithma1213@gmail.com>
2022-12-05 10:51:24 +0530

    modules and Pip

---
+++ Modules and Pip/modules-usage.py
@@ -0,0 +1,10 @@
+import useful_tools
+import docx
+
+print(useful_tools.roll_dice(10))
+
+# Python stores its External modules in - External Libraries folder like as node modules in React
+# To install external libraries we can use - pip install with relevant command same as npm install in react
+
+# When importing the installed pip install python-docx library you can write -> import docx at the top of the file
+docx.Document()
