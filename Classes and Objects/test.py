9a879682749751af05aa94931361fde08824ea42
swee76 <oshinikithma1213@gmail.com>
2022-12-05 11:15:24 +0530

    Classes and Objects

---
+++ Classes and Objects/test.py
@@ -0,0 +1,8 @@
+from Student import Student
+
+student1 = Student("Sunil", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 2.1, True)
+
+print("Student1 Name: " + student1.name)
+
+print("Student2 gpa: " + str(student2.gpa))
