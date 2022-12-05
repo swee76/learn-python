482e1db04c1e8748a14d34dcc03c0bdc8f4cb583
swee76 <oshinikithma1213@gmail.com>
2022-12-05 12:10:59 +0530

    Object Functions

--- Classes and Objects/test.py
+++ Classes and Objects/test.py
@@ -6,3 +6,9 @@ student2 = Student("Pam", "Art", 2.1, True)
 print("Student1 Name: " + student1.name)
 
 print("Student2 gpa: " + str(student2.gpa))
+
+student3 = Student("Jam", "Science", 3.8, True)
+
+# Object Functions
+print(student1.on_honor_roll())
+print(student3.on_honor_roll())
